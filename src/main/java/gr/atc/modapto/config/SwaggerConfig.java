915c0029e9264e2b7f94682dec4d0c9b0bc4f128
AlkAzna <a.aznavouridis@atc.gr>
2025-02-07 16:30:41 +0200

    Fixed OpenAPI Spec, Version and CI/CD

--- src/main/java/gr/atc/modapto/config/SwaggerConfig.java
+++ src/main/java/gr/atc/modapto/config/SwaggerConfig.java
@@ -10,10 +10,17 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${build.version}")
+    private String appVersion;
+
     @Bean
     public OpenAPI openAPIDocumentation() {
         return new OpenAPI()
-                .info(new Info().title("Notifacation Center API").version("1.0").description("API documentation for Notifacation Center service"))
+                .info(new Info()
+                        .title("Notifacation Center API")
+                        .version(appVersion)
+                        .description("API documentation for Notifacation Center service"))
+                .openapi("3.0.3")
                 .components(new Components()
                         .addSecuritySchemes("bearerToken", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
