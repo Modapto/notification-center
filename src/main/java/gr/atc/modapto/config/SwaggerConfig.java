f6039b0e95f5fc2a8c5f6b41e6013c4bec20c887
AlkAzna <a.aznavouridis@atc.gr>
2025-03-31 11:22:40 +0300

    Minor fix on Swagger Config import libraries

--- src/main/java/gr/atc/modapto/config/SwaggerConfig.java
+++ src/main/java/gr/atc/modapto/config/SwaggerConfig.java
@@ -3,7 +3,7 @@ package gr.atc.modapto.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
-
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
